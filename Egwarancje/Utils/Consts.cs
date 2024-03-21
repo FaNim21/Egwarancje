f18530b53ddf9fe72664e9300a88a2d26eb800f3
FaNim21 <filipekbwo@wp.pl>
2024-03-21 14:48:47 +0100

    const

--- Egwarancje/Utils/Consts.cs
+++ Egwarancje/Utils/Consts.cs
@@ -3,4 +3,9 @@
 public class Consts
 {
     public const string Version = "v0.1.0";
+
+    public const string DbName = "";
+    public const string AddressIP = "";
+    public const string DbLoginUserName = "";
+    public const string DbLoginPassword = "";
 }
