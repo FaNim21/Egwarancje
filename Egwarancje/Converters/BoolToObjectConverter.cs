836a32526ede154d796e241656642debc7277db8
FaNim21 <filipekbwo@wp.pl>
2024-06-09 14:24:31 +0200

    radio buttons

---
+++ Egwarancje/Converters/BoolToObjectConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace Egwarancje.Converters;
+
+public class BoolToObjectConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue && parameter != null)
+            return boolValue == bool.Parse(parameter.ToString()!);
+
+        return false;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue && parameter != null)
+            return boolValue == bool.Parse(parameter.ToString()!);
+
+        return false;
+    }
+}
