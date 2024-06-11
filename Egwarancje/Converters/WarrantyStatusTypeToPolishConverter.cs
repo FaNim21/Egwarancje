3e8986ecfb065573339d404c8fe6f888ff002392
Karolinaagrab <karolinapl6@o2.pl>
2024-06-11 18:32:26 +0200

    Added WarrantyStatusTypeToPolishConverter

---
+++ Egwarancje/Converters/WarrantyStatusTypeToPolishConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+using EgwarancjeDbLibrary.Models;
+
+namespace Egwarancje.Converters
+{
+    public class WarrantyStatusTypeToPolishConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is WarrantyStatusType status)
+            {
+                switch (status)
+                {
+                    case WarrantyStatusType.Awaitng:
+                        return "Oczekuje";
+                    case WarrantyStatusType.Accepted:
+                        return "Zaakceptowane";
+                    case WarrantyStatusType.Declined:
+                        return "Odrzucone";
+                }
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
