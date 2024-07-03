b510a2e933c6671f4ad69ef3179e620166bc1c11
FaNim21 <filipekbwo@wp.pl>
2024-07-03 11:43:00 +0200

    test push for dc

--- Egwarancje/ViewModels/WarrantyDetailsViewModel.cs
+++ Egwarancje/ViewModels/WarrantyDetailsViewModel.cs
@@ -6,26 +6,6 @@ using System.Collections.ObjectModel;
 
 namespace Egwarancje.ViewModels;
 
-public partial class WarrantySpecDetail : BaseViewModel
-{
-    [ObservableProperty] private bool isVisible;
-    [ObservableProperty] private WarrantySpec spec;
-    [ObservableProperty] private ObservableCollection<WarrantySpecAttachmentViewModel> attachments = [];
-
-    public WarrantySpecDetail(WarrantySpec spec)
-    {
-        this.spec = spec;
-
-        if (spec.Attachments == null || spec.Attachments.Count == 0) return;
-        for (int i = 0; i < spec.Attachments.Count; i++)
-        {
-            var current = spec.Attachments[i];
-            WarrantySpecAttachmentViewModel attachmentViewModel = new(current);
-            attachments.Add(attachmentViewModel);
-        }
-    }
-}
-
 public partial class WarrantyDetailsViewModel : BaseViewModel
 {
     private readonly Warranty warranty;
---
+++ Egwarancje/ViewModels/WarrantySpecDetail.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using EgwarancjeDbLibrary.Models;
+using System.Collections.ObjectModel;
+
+namespace Egwarancje.ViewModels;
+
+public partial class WarrantySpecDetail : BaseViewModel
+{
+    [ObservableProperty] private bool isVisible;
+    [ObservableProperty] private WarrantySpec spec;
+    [ObservableProperty] private ObservableCollection<WarrantySpecAttachmentViewModel> attachments = [];
+
+    public WarrantySpecDetail(WarrantySpec spec)
+    {
+        this.spec = spec;
+
+        if (spec.Attachments == null || spec.Attachments.Count == 0) return;
+        for (int i = 0; i < spec.Attachments.Count; i++)
+        {
+            var current = spec.Attachments[i];
+            WarrantySpecAttachmentViewModel attachmentViewModel = new(current);
+            attachments.Add(attachmentViewModel);
+        }
+    }
+}
