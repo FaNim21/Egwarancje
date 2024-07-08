5035fc63b61638ee7804a362be2018e0969f85ed
Karolina Tal <karo.talarek2208@gmail.com>
2024-07-08 12:47:26 +0200

    changed gitignore for dbconsts

--- EgwarancjeAPI/DbConsts.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EgwarancjeAPI;
-
-public class DbConsts
-{
-    public const string DbName = "egwarancje3";
-    public const string AddressIP = "192.168.0.165";
-    public const string DbLoginUserName = "fanim2";
-    public const string DbLoginPassword = "fanim123";
-}
