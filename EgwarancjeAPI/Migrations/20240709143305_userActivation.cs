b3898fbfc22ec2d02bb235f9c122c739acbb9f77
FaNim21 <filipekbwo@wp.pl>
2024-07-09 16:49:29 +0200

    Added new column for user table to activate account and changed some things in sending mails

---
+++ EgwarancjeAPI/Migrations/20240709143305_userActivation.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EgwarancjeAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class userActivation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActivated",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActivated",
+                table: "Users");
+        }
+    }
+}
